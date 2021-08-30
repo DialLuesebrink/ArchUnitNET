8ccb6c6ba8e37251b03e4e5958dccb6ad96e4170
Travis CI <travis@travis-ci.org>
2021-08-30 22:18:27 +0000

    Deploy code docs to GitHub Pages Travis build: 381
    Commit: a3355d33624cdd754f1c8f368cde8ffe01d77828

---
+++ navtreedata.js
@@ -0,0 +1,34 @@
+var NAVTREE =
+[
+  [ "API Documentation", "index.html", [
+    [ "Packages", null, [
+      [ "Packages", "namespaces.html", "namespaces" ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Functions", "functions_func.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"classArchUnitNET_1_1Domain_1_1Interface.html#a327a83480f5c51dc4e3d97e19009f5b9",
+"classArchUnitNET_1_1Fluent_1_1Conditions_1_1SimpleCondition.html",
+"classArchUnitNET_1_1Fluent_1_1Syntax_1_1Elements_1_1GivenObjectsThat.html#ad55278882e2e3298e6bfc15e0e90b7a7",
+"classArchUnitNET_1_1Fluent_1_1Syntax_1_1Elements_1_1Members_1_1PropertyMembers_1_1GivenPropertyMembers.html#a1b08c4241d5fb37905dcedc61cb329bf",
+"classArchUnitNET_1_1Fluent_1_1Syntax_1_1Elements_1_1ObjectsShould.html#af142edb09ba744acbb675f4d3da9acfa",
+"classArchUnitNET_1_1Fluent_1_1Syntax_1_1Elements_1_1Types_1_1GivenTypesThat.html#aaecf2648e31406e3f594416c618cc531",
+"interfaceArchUnitNET_1_1Fluent_1_1IArchRuleCreator.html",
+"interfaceArchUnitNET_1_1Fluent_1_1Syntax_1_1Elements_1_1IObjectPredicates.html#ac33d9aa455340b1cab306d974f7bafac",
+"interfaceArchUnitNET_1_1Fluent_1_1Syntax_1_1Elements_1_1Types_1_1Classes_1_1IClassPredicates.html#a3dff95c46321781927703a65ab474aa8"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
